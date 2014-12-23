f006a5cf7c028e70c78b950e2b9841fc0f3f6a5c
Fritz Lin <uxfritz@163.com>
2014-12-24 00:36:53 +0800

    order list & order_detail

---
+++ user.php
@@ -0,0 +1,320 @@
+<?php
+
+/**
+ * ECSHOP 用户中心
+ * ============================================================================
+ * * 版权所有 2005-2012 上海商派网络科技有限公司，并保留所有权利。
+ * 网站地址: http://www.ecshop.com；
+ * ----------------------------------------------------------------------------
+ * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和
+ * 使用；不允许对程序代码以任何形式任何目的的再发布。
+ * ============================================================================
+ * $Author: liubo $
+ * $Id: user.php 16643 2009-09-08 07:02:13Z liubo $
+*/
+
+define('IN_ECS', true);
+
+require(dirname(__FILE__) . '/includes/init.php');
+/* 载入语言文件 */
+require_once(ROOT_PATH . 'languages/' .$_CFG['lang']. '/user.php');
+
+$act = isset($_GET['act']) ? $_GET['act'] : '';
+
+/* 用户登陆 */
+if ($act == 'do_login')
+{
+    $user_name = !empty($_POST['username']) ? $_POST['username'] : '';
+    $pwd = !empty($_POST['pwd']) ? $_POST['pwd'] : '';
+    if (empty($user_name) || empty($pwd))
+    {
+        $login_faild = 1;
+    }
+    else
+    {
+        if ($user->check_user($user_name, $pwd) > 0)
+        {
+            $user->set_session($user_name);
+            $user->set_cookie($user_name);
+            update_user_info();
+            show_user_center();
+        }
+        else
+        {
+            $login_faild = 1;
+        }
+    }
+}
+
+elseif ($act == 'order_list')
+{
+    $record_count = $db->getOne("SELECT COUNT(*) FROM " .$ecs->table('order_info'). " WHERE user_id = {$_SESSION['user_id']}");
+    if ($record_count > 0)
+    {
+        include_once(ROOT_PATH . 'includes/lib_transaction.php');
+        $page_num = '10';
+        $page = !empty($_GET['page']) ? intval($_GET['page']) : 1;
+        $pages = ceil($record_count / $page_num);
+
+        if ($page <= 0)
+        {
+            $page = 1;
+        }
+        if ($pages == 0)
+        {
+            $pages = 1;
+        }
+        if ($page > $pages)
+        {
+            $page = $pages;
+        }
+        $pagebar = get_wap_pager($record_count, $page_num, $page, 'user.php?act=order_list', 'page');
+        $smarty->assign('pagebar' , $pagebar);
+        /* 订单状态 */
+        $_LANG['os'][OS_UNCONFIRMED] = '未确认';
+        $_LANG['os'][OS_CONFIRMED] = '已确认';
+        $_LANG['os'][OS_SPLITED] = '已确认';
+        $_LANG['os'][OS_SPLITING_PART] = '已确认';
+        $_LANG['os'][OS_CANCELED] = '已取消';
+        $_LANG['os'][OS_INVALID] = '无效';
+        $_LANG['os'][OS_RETURNED] = '退货';
+
+        $_LANG['ss'][SS_UNSHIPPED] = '未发货';
+        $_LANG['ss'][SS_PREPARING] = '配货中';
+        $_LANG['ss'][SS_SHIPPED] = '已发货';
+        $_LANG['ss'][SS_RECEIVED] = '收货确认';
+        $_LANG['ss'][SS_SHIPPED_PART] = '已发货(部分商品)';
+        $_LANG['ss'][SS_SHIPPED_ING] = '配货中'; // 已分单
+
+        $_LANG['ps'][PS_UNPAYED] = '未付款';
+        $_LANG['ps'][PS_PAYING] = '付款中';
+        $_LANG['ps'][PS_PAYED] = '已付款';
+        $_LANG['cancel'] = '取消订单';
+        $_LANG['pay_money'] = '付款';
+        $_LANG['view_order'] = '查看订单';
+        $_LANG['received'] = '确认收货';
+        $_LANG['ss_received'] = '已完成';
+        $_LANG['confirm_received'] = '你确认已经收到货物了吗？';
+        $_LANG['confirm_cancel'] = '您确认要取消该订单吗？取消后此订单将视为无效订单';
+
+        $orders = get_user_orders($_SESSION['user_id'], $page_num, $page_num * ($page - 1));
+        if (!empty($orders))
+        {
+            foreach ($orders as $key => $val)
+            {
+                $orders[$key]['total_fee'] = encode_output($val['total_fee']);
+            }
+        }
+        //$merge  = get_user_merge($_SESSION['user_id']);
+
+        $smarty->assign('orders', $orders);
+    }
+    $smarty->assign('footer', get_footer());
+    $smarty->display('order_list.html');
+    exit;
+}
+
+/* 取消订单 */
+elseif ($act == 'cancel_order')
+{
+    include_once(ROOT_PATH . 'includes/lib_transaction.php');
+    include_once(ROOT_PATH . 'includes/lib_order.php');
+
+    $order_id = isset($_GET['order_id']) ? intval($_GET['order_id']) : 0;
+    if (cancel_order($order_id, $_SESSION['user_id']))
+    {
+        ecs_header("Location: user.php?act=order_list\n");
+        exit;
+    }
+}
+
+
+/* 查看订单详情 */
+elseif ($act == 'order_detail')
+{
+    include_once(ROOT_PATH . 'includes/lib_transaction.php');
+    include_once(ROOT_PATH . 'includes/lib_payment.php');
+    include_once(ROOT_PATH . 'includes/lib_order.php');
+    include_once(ROOT_PATH . 'includes/lib_clips.php');
+
+    $order_id = isset($_GET['order_id']) ? intval($_GET['order_id']) : 0;
+
+    /* 订单详情 */
+    $order = get_order_detail($order_id, $_SESSION['user_id']);
+
+    if ($order === false)
+    {
+        ecs_header("Location: user.php?act=order_list\n");
+        exit();
+    }
+
+    /* 是否显示添加到购物车 */
+    if ($order['extension_code'] != 'group_buy' && $order['extension_code'] != 'exchange_goods')
+    {
+        $smarty->assign('allow_to_cart', 1);
+    }
+
+    /* 订单商品 */
+    $goods_list = order_goods($order_id);
+    foreach ($goods_list AS $key => $value)
+    {
+        $goods_list[$key]['market_price'] = price_format($value['market_price'], false);
+        $goods_list[$key]['goods_price']  = price_format($value['goods_price'], false);
+        $goods_list[$key]['subtotal']     = price_format($value['subtotal'], false);
+    }
+
+     /* 设置能否修改使用余额数 */
+    if ($order['order_amount'] > 0)
+    {
+        if ($order['order_status'] == OS_UNCONFIRMED || $order['order_status'] == OS_CONFIRMED)
+        {
+            $user = user_info($order['user_id']);
+            if ($user['user_money'] + $user['credit_line'] > 0)
+            {
+                $smarty->assign('allow_edit_surplus', 1);
+                $smarty->assign('max_surplus', sprintf($_LANG['max_surplus'], $user['user_money']));
+            }
+        }
+    }
+
+    /* 未发货，未付款时允许更换支付方式 */
+    if ($order['order_amount'] > 0 && $order['pay_status'] == PS_UNPAYED && $order['shipping_status'] == SS_UNSHIPPED)
+    {
+        $payment_list = available_payment_list(false, 0, true);
+
+        /* 过滤掉当前支付方式和余额支付方式 */
+        if(is_array($payment_list))
+        {
+            foreach ($payment_list as $key => $payment)
+            {
+                if ($payment['pay_id'] == $order['pay_id'] || $payment['pay_code'] == 'balance')
+                {
+                    unset($payment_list[$key]);
+                }
+            }
+        }
+        $smarty->assign('payment_list', $payment_list);
+    }
+
+    /* 订单 支付 配送 状态语言项 */
+    $order['order_status'] = $_LANG['os'][$order['order_status']];
+    $order['pay_status'] = $_LANG['ps'][$order['pay_status']];
+    $order['shipping_status'] = $_LANG['ss'][$order['shipping_status']];
+
+    $smarty->assign('order',      $order);
+    $smarty->assign('goods_list', $goods_list);
+    $smarty->display('order_detail.html');
+}
+
+
+/* 确认收货 */
+elseif ($act == 'affirm_received')
+{
+    include_once(ROOT_PATH . 'includes/lib_transaction.php');
+
+    $order_id = isset($_GET['order_id']) ? intval($_GET['order_id']) : 0;
+    $_LANG['buyer'] = '买家';
+    if (affirm_received($order_id, $_SESSION['user_id']))
+    {
+        ecs_header("Location: user.php?act=order_list\n");
+        exit;
+    }
+
+}
+
+/* 退出会员中心 */
+elseif ($act == 'logout')
+{
+    if (!isset($back_act) && isset($GLOBALS['_SERVER']['HTTP_REFERER']))
+    {
+        $back_act = strpos($GLOBALS['_SERVER']['HTTP_REFERER'], 'user.php') ? './index.php' : $GLOBALS['_SERVER']['HTTP_REFERER'];
+    }
+
+    $user->logout();
+    $Loaction = 'index.php';
+    ecs_header("Location: $Loaction\n");
+
+}
+/* 显示会员注册界面 */
+elseif ($act == 'register')
+{
+    if (!isset($back_act) && isset($GLOBALS['_SERVER']['HTTP_REFERER']))
+    {
+        $back_act = strpos($GLOBALS['_SERVER']['HTTP_REFERER'], 'user.php') ? './index.php' : $GLOBALS['_SERVER']['HTTP_REFERER'];
+    }
+
+    /* 取出注册扩展字段 */
+    $sql = 'SELECT * FROM ' . $ecs->table('reg_fields') . ' WHERE type < 2 AND display = 1 ORDER BY dis_order, id';
+    $extend_info_list = $db->getAll($sql);
+    $smarty->assign('extend_info_list', $extend_info_list);
+    /* 密码找回问题 */
+    $_LANG['passwd_questions']['friend_birthday'] = '我最好朋友的生日？';
+    $_LANG['passwd_questions']['old_address']     = '我儿时居住地的地址？';
+    $_LANG['passwd_questions']['motto']           = '我的座右铭是？';
+    $_LANG['passwd_questions']['favorite_movie']  = '我最喜爱的电影？';
+    $_LANG['passwd_questions']['favorite_song']   = '我最喜爱的歌曲？';
+    $_LANG['passwd_questions']['favorite_food']   = '我最喜爱的食物？';
+    $_LANG['passwd_questions']['interest']        = '我最大的爱好？';
+    $_LANG['passwd_questions']['favorite_novel']  = '我最喜欢的小说？';
+    $_LANG['passwd_questions']['favorite_equipe'] = '我最喜欢的运动队？';
+    /* 密码提示问题 */
+    $smarty->assign('passwd_questions', $_LANG['passwd_questions']);
+    $smarty->assign('footer', get_footer());
+    $smarty->display('user_passport.html');
+}
+/* 注册会员的处理 */
+elseif ($act == 'act_register')
+{
+        include_once(ROOT_PATH . 'includes/lib_passport.php');
+
+        $username = isset($_POST['username']) ? trim($_POST['username']) : '';
+        $password = isset($_POST['password']) ? trim($_POST['password']) : '';
+        $email    = isset($_POST['email']) ? trim($_POST['email']) : '';
+        $other['msn'] = isset($_POST['extend_field1']) ? $_POST['extend_field1'] : '';
+        $other['qq'] = isset($_POST['extend_field2']) ? $_POST['extend_field2'] : '';
+        $other['office_phone'] = isset($_POST['extend_field3']) ? $_POST['extend_field3'] : '';
+        $other['home_phone'] = isset($_POST['extend_field4']) ? $_POST['extend_field4'] : '';
+        $other['mobile_phone'] = isset($_POST['extend_field5']) ? $_POST['extend_field5'] : '';
+        $sel_question = empty($_POST['sel_question']) ? '' : compile_str($_POST['sel_question']);
+        $passwd_answer = isset($_POST['passwd_answer']) ? compile_str(trim($_POST['passwd_answer'])) : '';
+
+        $back_act = isset($_POST['back_act']) ? trim($_POST['back_act']) : '';
+
+        if (m_register($username, $password, $email, $other) !== false)
+        {
+            /*把新注册用户的扩展信息插入数据库*/
+            $sql = 'SELECT id FROM ' . $ecs->table('reg_fields') . ' WHERE type = 0 AND display = 1 ORDER BY dis_order, id';   //读出所有自定义扩展字段的id
+            $fields_arr = $db->getAll($sql);
+
+            $extend_field_str = '';    //生成扩展字段的内容字符串
+            foreach ($fields_arr AS $val)
+            {
+                $extend_field_index = 'extend_field' . $val['id'];
+                if(!empty($_POST[$extend_field_index]))
+                {
+                    $temp_field_content = strlen($_POST[$extend_field_index]) > 100 ? mb_substr($_POST[$extend_field_index], 0, 99) : $_POST[$extend_field_index];
+                    $extend_field_str .= " ('" . $_SESSION['user_id'] . "', '" . $val['id'] . "', '" . compile_str($temp_field_content) . "'),";
+                }
+            }
+            $extend_field_str = substr($extend_field_str, 0, -1);
+
+            if ($extend_field_str)      //插入注册扩展数据
+            {
+                $sql = 'INSERT INTO '. $ecs->table('reg_extend_info') . ' (`user_id`, `reg_field_id`, `content`) VALUES' . $extend_field_str;
+                $db->query($sql);
+            }
+
+            /* 写入密码提示问题和答案 */
+            if (!empty($passwd_answer) && !empty($sel_question))
+            {
+                $sql = 'UPDATE ' . $ecs->table('users') . " SET `passwd_question`='$sel_question', `passwd_answer`='$passwd_answer'  WHERE `user_id`='" . $_SESSION['user_id'] . "'";
+                $db->query($sql);
+            }
+
+            $ucdata = empty($user->ucdata)? "" : $user->ucdata;
+            $Loaction = 'index.php';
+            ecs_header("Location: $Loaction\n");
+        }
+}
+
+?>
